c14b56cc2634f7b408c8d19315edaffab0c9e21d
Andy Pierce <redjet736@gmail.com>
2013-04-28 13:23:00 -0700

    Added edit / delete to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
   
   def index
@@ -25,6 +26,17 @@ class UsersController < ApplicationController
   end
   
   def destroy
-    # TO IMPLEMENT
+    User.find(params[:id]).destroy()
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
 end
