c816207c05871d07c214dfeb7ccad58224f67f2d
Jack Chan <jchan172@gmail.com>
2013-04-30 21:46:24 -0700

    Added some comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 	end
 
 	def create # http POST request
-		# once login button is pushed, it sends a POST request, which brings us here
+		# once login button is pushed, it sends a POST request, which brings us here.
 		# we have access to the username and password that the user entered
 		# by getting params[:session][:username] and params[:session][:password]
 
@@ -13,8 +13,8 @@ class SessionsController < ApplicationController
 		user = Users.find_by_username(params[:session][:username])
 		# if there's a user and we can authenticate with password
 		if user && user.authenticate(params[:session][:password])
-			sign_in user # call a helper method to sign in the user
-			redirect_to '/dashboard' # bring the user to his page
+			sign_in user # call a helper method to sign in the user (need to add method ourselves)
+			redirect_to '/dashboard' # bring the user to his page (uses standard rails method)
 		else # otherwise, a user doesn't exist or the password wasn't right
 			flash.now[:error] = "Username/password combination is not correct"
 			# rerender the login page. the flash.now call will set a banner to display
@@ -23,8 +23,8 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy # http DELETE request
-		sign_out
-		redirect_to root_url
+		sign_out # calls a helper method to sign out the user (need to add method ourselves)
+		redirect_to root_url # redirect to home page (uses standard rails method)
 	end
 
 	# this is the dashboard where the user can manage content
@@ -32,7 +32,7 @@ class SessionsController < ApplicationController
 		if !signed_in
 			redirect_to 'new'
 		else
-			
+			# probably need to do some stuff
 		end
 	end
 end
