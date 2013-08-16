f8e970241b7cc6b8c4fa2de44719c66a9c644d12
Jack Chan <jchan172@gmail.com>
2013-08-15 19:31:40 -0700

    Removed more redactor_rails stuff

--- db/schema.rb
+++ db/schema.rb
@@ -49,23 +49,6 @@ ActiveRecord::Schema.define(version: 20130803235239) do
     t.boolean  "draft",      default: false
   end
 
-  create_table "redactor_assets", force: true do |t|
-    t.integer  "user_id"
-    t.string   "data_file_name",               null: false
-    t.string   "data_content_type"
-    t.integer  "data_file_size"
-    t.integer  "assetable_id"
-    t.string   "assetable_type",    limit: 30
-    t.string   "type",              limit: 30
-    t.integer  "width"
-    t.integer  "height"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
-  end
-
-  add_index "redactor_assets", ["assetable_type", "assetable_id"], name: "idx_redactor_assetable", using: :btree
-  add_index "redactor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_redactor_assetable_type", using: :btree
-
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
