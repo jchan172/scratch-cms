79b3500b01817e84895cc6437a7b9551663bf4b5
Jack Chan <jchan172@gmail.com>
2013-08-19 19:09:07 -0700

    Polished front page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   def home
+  	@quadrotor = User.first.projects.find_by_title('Radiation Mapping')
+  	@gallery = User.first.projects.find_by_title('Photography')
+  	@ddr = User.first.projects.find_by_title('DDR')
+  	@amp = User.first.projects.find_by_title('Pocket Amplifier')
   end
 
   def about
