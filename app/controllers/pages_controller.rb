3afe76876ddd56becde1fd82a354638ec9968417
Jack Chan <jchan172@gmail.com>
2013-10-01 19:11:42 -0700

    Added photography gallery

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,6 @@ class PagesController < ApplicationController
     @pcb = User.first.projects.find_by_title('PoE Measurement Board')
     @scratchcms = User.first.projects.find_by_title('Scratch CMS')
   	@quadrotor = User.first.projects.find_by_title('Radiation Mapping')
-  	@gallery = User.first.projects.find_by_title('Photography')
   	@ddr = User.first.projects.find_by_title('DDR')
   	@amp = User.first.projects.find_by_title('Pocket Amplifier')
   end
